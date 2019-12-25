7fa30f7e939594f60ffcfd728eb5934d727f7de2
Rajesh singh <rajeshmah@hotmail.com>
2019-12-25 11:40:48 +0545

    Uploding dicee app to git

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(DiceMain());
+
+class DiceMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dicee"),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int firstDiceImg = 1;
+  int secondDiceImg = 1;
+  void changedice() {
+    setState(
+      () {
+        firstDiceImg = Random().nextInt(6) + 1;
+        secondDiceImg = Random().nextInt(6) + 1;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print("first button was press");
+                changedice();
+              },
+              child: Image.asset(
+                "images/dice$firstDiceImg.png",
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print("second button was press");
+                changedice();
+              },
+              child: Image.asset(
+                "images/dice$secondDiceImg.png",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
